370764966b6f3281afe1ab52aec8fa7454c7f303
wikwoj <wikwoj0512@gmail.com>
2024-06-12 17:32:43 +0200

    changed structures

--- models/session.py
+++ models/session.py
@@ -13,22 +13,24 @@ class Session:
         socketio.emit('data', points, namespace='/', to=self.session_id)
 
     def configure(self, data):
-        self.fields = []
-        for el in data.get("data", []):
-            if not el:
-                continue
-            origin = el.get('origin')
-            keys = el.get('keys')
-            if origin is None or keys is None or not isinstance(origin, int) or not isinstance(keys, list):
-                return "Wrong request structure {origin: string, keys: string[]}[]"
-            for key in keys:
-                if not isinstance(key, str):
-                    return "Wrong request structure {origin: string, keys: string[]}[]"
-            self.fields.append({'origin': origin, 'keys': keys})
+        # setting origins
+        origins = data.get('origins')
+        if origins is not None and isinstance(origins, dict):
+            fields = []
+            for origin, keys in origins.items():
+                if not isinstance(keys, list):
+                    continue
+                if not isinstance(origin, int):
+                    continue
+                fields.append({'origin': origin, 'keys': keys})
+            self.fields = fields
+
+        # setting timeframe
         try:
             self.timeframe = float(data.get("timeframe", self.timeframe))
         except Exception as e:
             print(f"Failed to set timeframe for data {data}: {e}")
+        # setting points
         try:
             self.points = int(data.get("points", self.points))
         except Exception as e:
@@ -36,18 +38,19 @@ class Session:
         return None
 
     def get_points(self, data):
-        ret_data = []
+        ret_data = {}
         for field in self.fields:
             origin = field.get('origin')
             keys = field.get('keys')
+            ret_data[origin] = {}
             for key in keys:
                 points = data.get(origin).get(key)
                 if not len(points):
-                    ret_data.append({'values': [], 'origin': origin, 'key': key})
+                    ret_data[origin][key] = []
+                    continue
                 timestamp = points[-1].timestamp - self.timeframe
-
                 points = get_data_points(points, timestamp)
                 points = largest_triangle_three_buckets(points, self.points)
                 points = [[point.timestamp, point.value] for point in points]
-                ret_data.append({'values': points, 'origin': origin, 'key': key})
+                ret_data[origin][key] = points
         return ret_data
