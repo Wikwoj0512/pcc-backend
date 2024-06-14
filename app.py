737a2b5fda8d741c6a3f067cf13c334f6480e01f
wikwoj <wikwoj0512@gmail.com>
2024-06-14 23:36:56 +0200

    added configuration options

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import json
 import os
 import signal
-import sys
 import threading
 
 from models import MqttReciever
@@ -10,6 +9,8 @@ from flask import Flask, request
 from flask_socketio import SocketIO
 from flask_cors import CORS, cross_origin
 
+from utils import Config
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secret!'
@@ -17,7 +18,9 @@ socketio = SocketIO(app, cors_allowed_origins=['http://localhost:5173'], async_m
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-reciever = MqttReciever()
+config = Config()
+
+reciever = MqttReciever(config.reciever_config, config.mqtt_host, config.mqtt_port, config.mqtt_topic)
 
 socketio.start_background_task(reciever.infinite_sender, socketio)
 
@@ -60,7 +63,8 @@ def my_disconnect():
 
 
 if __name__ == '__main__':
-    socketio_thread = threading.Thread(target=socketio.run, args=(app,), kwargs={"debug": False, "host": "0.0.0.0"}, daemon=True)
+    socketio_thread = threading.Thread(target=socketio.run, args=(app,),
+                                       kwargs={"debug": False, "host": "0.0.0.0", "port": config.pcc_port}, daemon=True)
     socketio_thread.start()
     try:
         while True:
---
+++ utils/config.py
@@ -0,0 +1,50 @@
+import argparse
+
+import yaml
+
+
+class Config:
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            prog='Pcc backend',
+            description='Running without args will load default config, use --config to specify config file or --console to specify command line arguments')
+
+        parser.add_argument('--config', help="Config file name")
+        parser.add_argument('--console', action='store_true')
+        parser.add_argument('--pcc-port', help="port for pcc")
+        parser.add_argument('--mqtt-topic', help='pcc in topic name')
+        parser.add_argument('--mqtt-host', help='mqtt host')
+        parser.add_argument('--mqtt-port', help='mqtt port')
+        parser.add_argument('--reciever-config', help="reciever config file")
+        args = parser.parse_args()
+
+        if args.console:
+            self.analyze_console(args)
+            return
+
+        self.analyze_config_file(args.config if args.config else 'app_config.yaml')
+
+    def analyze_console(self, args):
+        self.pcc_port = int(args.pcc_port) if args.pcc_port else 2137
+        self.mqtt_topic = args.mqtt_topic if args.mqtt_topic else 'pcc/in'
+        self.mqtt_host = args.mqtt_host if args.mqtt_host else 'localhost'
+        self.mqtt_port = int(args.mqtt_port) if args.mqtt_port else 1883
+        self.reciever_config = args.reciever_config if args.reciever_config else 'config.json'
+
+    def analyze_config_file(self, config_file):
+        try:
+            with open(config_file) as f:
+                contents = yaml.safe_load(f)
+            print(contents, type(contents))
+            self.pcc_port = int(contents.get('pcc-port', 2137))
+            self.mqtt_topic = contents.get('mqtt-topic', 'pcc/in')
+            self.mqtt_host = contents.get('mqtt-host', 'localhost')
+            self.mqtt_port = int(contents.get('mqtt-port', 1883))
+            self.reciever_config = contents.get('reciever-config', 'config.json')
+        except Exception as e:
+            print(e)
+            self.pcc_port = 2137
+            self.mqtt_topic = 'pcc/in'
+            self.mqtt_host = 'localhost'
+            self.mqtt_port = 1883
+            self.reciever_config = 'config.json'
